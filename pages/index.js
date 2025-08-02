c336da65df446fb5eed3b2b56b8a64b724e5f13b
omarvikas <omarvikas@gmail.com>
2025-08-02 14:57:02 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Our Hyderabad Team Page</h1>
+      <p>This site showcases our Mindspark & PAL work in Telangana.</p>
+    </div>
+  );
+}
