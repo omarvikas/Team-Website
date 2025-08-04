c67b45ff4d1400cfb789422dea3e41a2c36c84ca
omarvikas <omarvikas@gmail.com>
2025-08-04 16:24:42 +0530

    Update Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -33,28 +33,28 @@ const Team = () => {
       bio: 'Managing day-to-day operations and providing technical support to ensure smooth functioning of all educational programs.',
       linkedin: '#anand-linkedin',
       email: 'anand.kumar@ei.study'
-    }
+    },
     {
       name: 'Pollepalli Ramesh',
       role: 'junior Executive',
       bio: 'Managing day-to-day operations and providing technical support to ensure smooth functioning of all educational programs.',
       linkedin: '#anand-linkedin',
       email: 'anand.kumar@ei.study'
-    }
+    },
     {
       name: 'Bheem Rao',
       role: 'Project Coordinator',
       bio: 'Managing day-to-day operations and providing technical support to ensure smooth functioning of all educational programs.',
       linkedin: '#anand-linkedin',
       email: 'anand.kumar@ei.study'
-    }
+    },
     {
       name: 'Nunavath Bansi',
       role: 'Project Coordinator',
       bio: 'Managing day-to-day operations and providing technical support to ensure smooth functioning of all educational programs.',
       linkedin: '#anand-linkedin',
       email: 'anand.kumar@ei.study'
-    }
+    },
      {
       name: 'Kishor Goli',
       role: 'Project Coordinator',
